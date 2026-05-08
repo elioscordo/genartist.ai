7a3e957d1f36e4df0eb5c70fdf4e5ee442febb8f
Elio <elioscordo@gmail.com>
2026-05-08 17:29:09 +0100

    contact form

--- brainstorm/views.py
+++ brainstorm/views.py
@@ -1,5 +1,6 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, TemplateView
+from django.contrib import messages
 from unfold.views import UnfoldModelAdminViewMixin
 from unfold.forms import UserCreationForm  # Use Unfold's styled form
 from .models import ContactRequest
@@ -8,7 +9,13 @@ class LandingView(CreateView):
     model = ContactRequest
     template_name = "landing.html"
     fields = ['name', 'email']
-    success_url = reverse_lazy("landing")
+    
+    def form_valid(self, form):
+        messages.success(self.request, "Thank you for your interest! We'll be in touch shortly.")
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("landing") + "#contact"
 
 
 class AdminSignupView(CreateView):
