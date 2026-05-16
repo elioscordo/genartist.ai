1f959f77ecdabdbaaa0bd546eb7a310b716a0673
Elio <elioscordo@gmail.com>
2026-05-16 02:57:38 +0100

    potegres

--- project/settings.py
+++ project/settings.py
@@ -251,8 +251,12 @@ UNFOLD = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME", "guillermo"),
+        'USER': os.getenv("DB_USER", "myuser"),
+        'PASSWORD': os.getenv("DB_PASSWORD", "mypassword"),
+        'HOST': os.getenv("DB_HOST", "localhost"),
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
 }
 
